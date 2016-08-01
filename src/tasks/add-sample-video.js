07825581bd1bb2bf8be14747b8bb0f9708210028
Luke Tillman <luke.tillman@gmail.com>
2016-08-01 11:44:26 -0600

    Track sample videos in Cassandra

--- src/tasks/add-sample-video.js
+++ src/tasks/add-sample-video.js
@@ -4,6 +4,7 @@ import { getGrpcClientAsync } from 'killrvideo-nodejs-common';
 import { VIDEO_CATALOG_SERVICE, VideoLocationType } from '../services/video-catalog';
 import { getSampleUserIdAsync, getUnusedYouTubeVideoAsync } from '../sample-data/get-sample-data';
 import { stringToUuid } from '../utils/protobuf-conversions';
+import { getCassandraClient } from '../utils/cassandra';
 
 /**
  * Adds a sample YouTube video to the video catalog.
@@ -19,9 +20,11 @@ export async function addSampleVideo() {
   if (video === null) throw new Error('No unused YouTube videos available');
   if (userId === null) throw new Error('No sample users available');
   
+  let videoId = uuid.v4();
+
   // Make Grpc request
   let request = {
-    videoId: stringToUuid(uuid.v4()),
+    videoId: stringToUuid(videoId),
     userId: stringToUuid(userId),
     name: video.name,
     description: video.description,
@@ -30,6 +33,10 @@ export async function addSampleVideo() {
   };
 
   await client.submitYouTubeVideoAsync(request);
+
+  // Save the video Id to Cassandra so we can use it for future sample data generation
+  let cass = getCassandraClient();
+  await cass.executeAsync('INSERT INTO videos (videoid) VALUES (?)', [ videoId ]);
 };
 
 export default addSampleVideo;
