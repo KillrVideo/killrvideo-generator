66fc5e3a4d190b950633e68e6b701efbd83e987a
Luke Tillman <luke.tillman@gmail.com>
2016-08-01 09:17:44 -0600

    Add sample video view impl

--- src/tasks/add-sample-video-view.js
+++ src/tasks/add-sample-video-view.js
@@ -1,14 +1,23 @@
+import Promise from 'bluebird';
 import { getGrpcClientAsync } from 'killrvideo-nodejs-common';
 import { STATS_SERVICE } from '../services/stats';
+import { getSampleVideoIdAsync } from '../utils/get-sample-data';
+import { stringToUuid } from '../utils/protobuf-conversions';
 
 /**
  * Adds a view to a video.
  */
-export function addSampleVideoView() {
-  return getGrpcClientAsync(STATS_SERVICE)
-    .then(client => {
-      throw new Error('Not implemented');
-    });
+export async function addSampleVideoView() {
+  let [ client, videoId ] = await Promise.all([
+    getGrpcClientAsync(STATS_SERVICE),
+    getSampleVideoIdAsync()
+  ]);
+
+  if (videoId === null) throw new Error('No sample videos available');
+
+  await client.recordPlaybackStartedAsync({
+    videoId: stringToUuid(videoId)
+  });
 };
 
 export default addSampleVideoView;
