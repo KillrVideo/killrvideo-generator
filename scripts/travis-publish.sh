1b47dc013c882893e9b1380966ede4237f926fcc
Luke Tillman <luke.tillman@gmail.com>
2016-08-06 10:06:04 -0600

    Be specific when skipping publish

--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -11,14 +11,24 @@ SOURCE_BRANCH=master
 DOCKER_IMAGE=luketillman/killrvideo-generator
 
 # If a pull request, not for the source branch, or not a build for a tag, don't publish
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]] || [[ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]] || [[ -z "$TRAVIS_TAG" ]]; then
-  echo "Skipping publish."
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then 
+  echo "Skipping publish for pull request"
+  exit 0
+fi
+
+if [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+  echo "Skipping publish for branch $TRAVIS_BRANCH"
+  exit 0
+fi
+
+if [ -z "$TRAVIS_TAG" ]; then
+  echo "Skipping publish for non-tag build"
   exit 0
 fi
 
 # Make sure we have a user/pass
 if [[ -z "$DOCKER_USER" ]] || [[ -z "$DOCKER_PASS" ]]; then
-  echo "DOCKER_USER or DOCKER_PASS not set."
+  echo "DOCKER_USER or DOCKER_PASS not set"
   exit 1
 fi
 
