c7f533835c39cbb455de6be02945117162335e2c
Luke Tillman <luke.tillman@gmail.com>
2016-08-11 15:42:31 -0600

    Add default YouTube key to travis build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,4 +2,4 @@
 
 # Get the package version from the package.json then build with docker build
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-docker build -t luketillman/killrvideo-generator:$PACKAGE_VERSION .
+docker build -t luketillman/killrvideo-generator:$PACKAGE_VERSION --build-arg KILLRVIDEO_YOUTUBE_API_KEY=$KILLRVIDEO_YOUTUBE_API_KEY .
