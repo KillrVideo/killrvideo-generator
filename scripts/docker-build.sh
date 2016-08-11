8278c08a6482be13b1958de0814d9c1b6b4d1e0c
Luke Tillman <luke.tillman@gmail.com>
2016-08-11 16:30:46 -0600

    Switch to killrvideo org for docker images

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,4 +2,4 @@
 
 # Get the package version from the package.json then build with docker build
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-docker build -t luketillman/killrvideo-generator:$PACKAGE_VERSION --build-arg KILLRVIDEO_YOUTUBE_API_KEY=$KILLRVIDEO_YOUTUBE_API_KEY .
+docker build -t killrvideo/killrvideo-generator:$PACKAGE_VERSION --build-arg KILLRVIDEO_YOUTUBE_API_KEY=$KILLRVIDEO_YOUTUBE_API_KEY .
