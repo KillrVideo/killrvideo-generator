362f02f54560dadb375466ff1341736277b4eaa2
Luke Tillman <luke.tillman@gmail.com>
2016-09-23 10:46:35 -0600

    Remove cassandra dependency on common
    - Move copies of relevant functions into existing cassandra util file

--- src/utils/cassandra.js
+++ src/utils/cassandra.js
@@ -1,6 +1,8 @@
 import Promise from 'bluebird';
 import config from 'config';
-import { logger, createKeyspaceIfNotExistsAsync, getCassandraClientAsync } from 'killrvideo-nodejs-common';
+import { Client, types as CassandraTypes } from 'cassandra-driver';
+import { logger } from './logging';
+import { lookupServiceAsync } from './lookup-service';
 
 /**
  * An array of CQL table strings to use for the schema.
@@ -38,6 +40,59 @@ function getCassandraConfig() {
   const { keyspace, replication } = config.get('cassandra');
   return { keyspace, replication };
 }
+
+// Client promises by keyspace
+const clientPromises = new Map();
+
+/**
+ * Gets a Cassandra client instance that is connected to the specified keyspace.
+ */
+export function getCassandraClientAsync(keyspace) {
+  if (clientPromises.has(keyspace)) {
+    return clientPromises.get(keyspace);
+  }
+    
+  const promise = lookupServiceAsync('cassandra')
+    .then(contactPoints => {
+      let clientOpts = {
+        contactPoints,
+        queryOptions: { 
+          prepare: true,
+          consistency: CassandraTypes.consistencies.localQuorum
+        }
+      };
+      
+      if (keyspace) {
+        clientOpts.keyspace = keyspace;
+      }
+      
+      // Create a client and promisify it
+      let client = new Client(clientOpts);
+      client = Promise.promisifyAll(client);
+      
+      // Connect and return the connected client
+      return client.connectAsync().return(client);
+    })
+    .catch(err => {
+      clientPromises.delete(keyspace);
+      throw err;
+    });
+  
+  clientPromises.set(keyspace, promise);
+  return promise;
+};
+
+/**
+ * Creates a keyspace in Cassandra if it doesn't already exist. Pass the name of the keyspace and the
+ * string to be used as the REPLICATION setting (i.e. after WITH REPLIACTION = ...).
+ */
+function createKeyspaceIfNotExistsAsync(keyspace, replication) {
+  // Create CQL
+  const cql = `CREATE KEYSPACE IF NOT EXISTS ${keyspace} WITH REPLICATION = ${replication}`;
+  
+  // Get a client, then create the keyspace
+  return getCassandraClientAsync().then(client => client.executeAsync(cql));
+};
 
 /**
  * Create the tables if they don't already exist.
