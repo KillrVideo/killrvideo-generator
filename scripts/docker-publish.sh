0e1981f58a9d65c0117a8ca100b2672e9236fce8
Aleksandr Volochnev <a.volochnev@gmail.com>
2019-01-23 23:00:58 +0100

    Reverted docker-publish.sh as not used for Travis CI

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -xu # Print debug output, fail on empty env vars
-
 # This script assumes the following environment variables are present
 #
 #   DOCKER_USER=username
 #   DOCKER_PASS=somesecretpassword
 DOCKER_IMAGE=killrvideo/killrvideo-generator
 
-set +e # Allow git describe to fail (which it will if this isn't a tagged commit)
+# Allow git describe to fail (which it will if this isn't a tagged commit)
+set +e 
 CURRENT_TAG=`git describe --tags --exact-match 2> /dev/null`
+
 set -e # Exit with nonzero exit code if anything fails
 
 # Only allow publish for tags
@@ -24,12 +24,8 @@ if [[ -z "$DOCKER_USER" ]] || [[ -z "$DOCKER_PASS" ]]; then
   exit 1
 fi
 
-docker tag $DOCKER_IMAGE $CURRENT_TAG
-
 echo "Publishing $DOCKER_IMAGE for git tag $CURRENT_TAG"
 
 # Login to Docker and push the image which should have been built
-set +x # Suppress credentials output. Travis CI promises to hide them but we should trust no one
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-set -x # Bring back output
-docker push $DOCKER_IMAGE:$CURRENT_TAG
+docker push $DOCKER_IMAGE
