684154f307d4f302d59b01b8940b5c8fb68db545
Aleksandr Volochnev <a.volochnev@gmail.com>
2019-01-23 23:06:00 +0100

    Travisisation

--- scripts/travis-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -xue # Debug output, fail on an empty env variable, exit on error
-
-docker build -t killrvideo/killrvideo-generator:${TRAVIS_COMMIT} --build-arg KILLRVIDEO_YOUTUBE_API_KEY=$KILLRVIDEO_YOUTUBE_API_KEY .
