47b5ef6b0bcc6759a36eb77b45b2836599928181
Aleksandr Volochnev <a.volochnev@gmail.com>
2019-01-23 22:45:54 +0100

    Code optimisation

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -2,4 +2,4 @@
 
 set -xue # Debug output, fail on an empty env variable, exit on error
 
-docker build -t killrvideo/killrvideo-generator:`git log -1 --pretty=format:%h` --build-arg KILLRVIDEO_YOUTUBE_API_KEY=$KILLRVIDEO_YOUTUBE_API_KEY .
+docker build -t killrvideo/killrvideo-generator:${TRAVIS_COMMIT} --build-arg KILLRVIDEO_YOUTUBE_API_KEY=$KILLRVIDEO_YOUTUBE_API_KEY .
--- scripts/travis-publish.sh
+++ scripts/travis-publish.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-set -xue # Print debug output, fail on empty env vars, exit on errors
+# Only allow publish for tags
+if [ -z "TRAVIS_TAG" ]; then
+  echo "Current commit is not for a tag, skipping publish"
+  exit 0
+fi
 
 # This script assumes the following environment variables are present
 if [[ -z "$DOCKER_USER" ]] || [[ -z "$DOCKER_PASS" ]]; then
@@ -8,20 +12,10 @@ if [[ -z "$DOCKER_USER" ]] || [[ -z "$DOCKER_PASS" ]]; then
   exit 1
 fi
 
+set -xue # Print debug output, fail on empty env vars, exit on errors
+
 DOCKER_IMAGE=killrvideo/killrvideo-generator
-TAGHASH=`git log -1 --pretty=format:%h`
-set +e; CURRENT_TAG=`git describe --tags --exact-match 2> /dev/null`; set -e
 
-# Only allow publish for tags
-if [ -z "$CURRENT_TAG" ]; then
-  echo "Current commit is not for a tag, skipping publish"
-  exit 0
-fi
-
-docker tag ${DOCKER_IMAGE}:${TAGHASH} ${DOCKER_IMAGE}:${CURRENT_TAG}
-
-echo "Publishing $DOCKER_IMAGE for git tag $CURRENT_TAG"
-
-# Login to Docker and push the image which should have been built
+docker tag ${DOCKER_IMAGE}:${TRAVIS_COMMIT} ${DOCKER_IMAGE}:${TRAVIS_TAG}
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push ${DOCKER_IMAGE}:${CURRENT_TAG}
